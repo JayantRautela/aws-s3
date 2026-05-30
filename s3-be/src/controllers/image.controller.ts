5eaa2f359f9a52d002809730129a9d0a7b5c276d
JayantRautela <jayantrautela0205@gmail.com>
2026-05-30 19:57:11 +0530

    add route to get image by id

--- s3-be/src/controllers/image.controller.ts
+++ s3-be/src/controllers/image.controller.ts
@@ -20,3 +20,39 @@ export const getImages = async (req: Request, res: Response) => {
     });
   }
 }
+
+export const getImageById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "ID is required to fetch image"
+      });
+    }
+
+    const image = await Image.findById(id);
+
+    if (!image) {
+      return res.status(400).json({
+        success: false,
+        message: "No image to show"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Image fetched successfully",
+      data: {
+        image: image
+      }
+    });
+  } catch (error) {
+    console.log("Error in getting the iamges :- ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error"
+    });
+  }
+}
--- s3-be/src/routes/image.route.ts
+++ s3-be/src/routes/image.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getImages } from "../controllers/image.controller.js";
+import { getImageById, getImages } from "../controllers/image.controller.js";
 
 const router = Router();
 
 router.get('/get-images', getImages);
+router.get('/get-image/:id', getImageById);
 
 export default router;
